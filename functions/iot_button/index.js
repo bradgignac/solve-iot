a16809b9a5ba6cbc78aa8c51e995e92b12c07318
Brad Gignac <bgignac@bradgignac.com>
2016-08-04 22:57:21 -0400

    Scaffold out IoT button handling.

--- functions/iot_button/index.js
+++ functions/iot_button/index.js
@@ -1,6 +1,29 @@
-console.log('starting function')
+console.log('Executing Lambda function.');
 
-exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e)
-  cb(null, { hello: 'world' })
+const email = (cb) => {
+  console.log('Sending email.');
+  cb(null);
+};
+
+const tweet = (cb) => {
+  console.log('Tweeting tweet.');
+  cb(null);
+};
+
+const strobe = (cb) => {
+  console.log('Strobing lights.');
+  cb(null);
+};
+
+exports.handle = (e, ctx, cb) => {
+  console.log('Processing Lambda event: %j', e);
+
+  switch (e.clickType) {
+    case 'SINGLE':
+      return email(cb);
+    case 'DOUBLE':
+      return tweet(cb);
+    case 'LONG':
+      return strobe(cb);
+  }
 }
